462d42ef994646b5d169ffc317dad2345856254c
Ksu <coder@intensol.com>
2011-11-02 14:16:13 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kadoinvest::Application.config.secret_token = '5225d3a5d2360a259041adbef14ac4df5d98773760ddceb71192633648fdc5a38a9858dadb48a5f09ea093df1df0006a2301db83ad4bb67b9fc2fda130f4e8f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kadoinvest::Application.config.session_store :cookie_store, :key => '_kadoinvest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kadoinvest::Application.config.session_store :active_record_store
